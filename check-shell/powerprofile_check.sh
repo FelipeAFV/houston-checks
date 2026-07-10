10340e4b1796db7bff90b3f6c0fccc785aaf46ec
FelipeAFV <ffigueroa@whitestack.com>
2026-07-10 08:14:52 -0400

    modifies script for power profile

--- check-shell/powerprofile_check.sh
+++ check-shell/powerprofile_check.sh
@@ -8,8 +8,8 @@ vendor=$(awk -F': ' '/vendor_id/ {print $2; exit}' /proc/cpuinfo)
 
 # AMD does not implement energy_perf_bias.
 if [[ "$vendor" == "AuthenticAMD" ]]; then
-    echo "AMD CPU detected."
-    echo "Check not applicable on AMD."
+    printf "AMD CPU detected."
+    printf "Check not applicable on AMD."
     printf 'CHECK_RC=0\n'
     exit 0
 fi
@@ -17,7 +17,7 @@ fi
 energy_perf_bias_file=/sys/devices/system/cpu/cpu0/power/energy_perf_bias
 
 if [[ ! -f "$energy_perf_bias_file" ]]; then
-  echo "Not AMD CPU but energy_perf_bias file not found."
+  printf "Not AMD CPU but energy_perf_bias file not found."
   printf 'CHECK_RC=0\n'
   exit 0
 fi
@@ -25,8 +25,8 @@ fi
 energy_perf_bias_value=$(cat "$energy_perf_bias_file")
 # Check if the file contains the expected value for the performance mode 
 if [[ "$energy_perf_bias_value" == "6" || "$energy_perf_bias_value" == "7" ]]; then
-  echo "Intel server"
-  echo "energy_perf_bias=$energy_perf_bias_value"
+  printf "Intel server"
+  printf "energy_perf_bias=%s" "$energy_perf_bias_value"
   printf 'CHECK_RC=0\n'
   exit 0
 fi
