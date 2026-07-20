db29e08b711f53ec9e8464900f840b6335f0920b
FelipeAFV <ffigueroa@whitestack.com>
2026-07-20 14:11:39 -0400

    Changes checks.yaml to adds variables per groups

--- rundeck/provisioning/scripts/src/rundeck/job_gen.py
+++ rundeck/provisioning/scripts/src/rundeck/job_gen.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Any
 
-from rundeck.provisioning.scripts.src.inventory.catalog import group_args
+from src.inventory.catalog import group_args
 from src.inventory.check_options import JobOption
 from src.configs.config import Config
 from src.utils.util import log
